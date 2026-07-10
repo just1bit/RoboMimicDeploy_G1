076c324bd35221af622f8b560b84bbe38e08b801
just1bit <69886231+just1bit@users.noreply.github.com>
2026-07-10 16:24:08 +1200

    fix standup if need after check

--- policy/loco_mode/LocoMode.py
+++ policy/loco_mode/LocoMode.py
@@ -122,7 +122,7 @@ class LocoMode(FSMState):
             return FSMStateName.SKILL_ASAP
         elif(self.state_cmd.skill_cmd == FSMCommand.STAND_UP):
             self.state_cmd.skill_cmd = FSMCommand.INVALID
-            if self.gravity_orientation[2] < 0.4:
+            if abs(self.gravity_orientation[2]) < 0.4:
                 return FSMStateName.STANDMODE
             else:
                 return FSMStateName.LOCOMODE
